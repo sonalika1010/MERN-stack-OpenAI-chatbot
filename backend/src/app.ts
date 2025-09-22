69243499233f2bb39e4e6c582a00fd8f7c478085
Sonalika Pradhan <sonalikapradhan2k03@gmail.com>
2025-09-22 15:52:21 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,8 +13,6 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://localhost:3000", 
-    "https://frontend-1h394gmkw-sonalikas-projects.vercel.app/"
   ],
   credentials: true,
 }));
